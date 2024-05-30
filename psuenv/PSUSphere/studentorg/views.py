c1963a19e5095e526536c4ffe0807c2e0b9d10af
Steve Oikawa <154987863+queanna20@users.noreply.github.com>
2024-05-30 14:10:32 +0800

    update again
    -anthony

--- psuenv/PSUSphere/studentorg/views.py
+++ psuenv/PSUSphere/studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
@@ -12,3 +15,20 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5 
+
+class OrganizationCreateView (CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_edit.html'
+    success_url = reverse_lazy('organizaton-list')
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organizaton-list')
