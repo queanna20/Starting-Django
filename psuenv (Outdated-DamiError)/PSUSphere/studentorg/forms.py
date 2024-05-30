b8d1e13170acae525d986fbd7258f588c0355426
Steve Oikawa <132885699+NyxOikawa@users.noreply.github.com>
2024-05-30 18:41:57 +0800

    update
    Co-Authored-By: Queanna Brittany <154987863+queanna20@users.noreply.github.com>

--- psuenv (Outdated-DamiError)/PSUSphere/studentorg/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from .models import Organization
-
-class OrganizationForm(ModelForm):
-    class Meta:
-        model = Organization
-        fields = "__all__"
